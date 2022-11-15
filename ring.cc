05c2435b9f23602ef9a4755f5d0acabc68ca3fea
Denes Fekete <feketden@cvut.cz>
2022-11-15 22:29:01 +0100

    progress: reading topologies from file

--- ring.cc
+++ ring.cc
@@ -12,21 +12,33 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	const auto readInputNumber = [](const string& p_message)
-	{
-		unsigned value;
-		cout << p_message;
-		cin >> value;
-		cout << endl;
-		return value;
-	};
+    ifstream infile("topologies.txt");
+    string line;
+    vector<vector<unsigned>> topologies;
+    
+    const unsigned testSize = 1;
+    auto numberOfTests = 0u;
 
-	const auto nodeCount = readInputNumber("Insert number of nodes: ");
+    while (getline(infile, line) && numberOfTests < testSize)
+    {
+        std::cout << line << std::endl;
+        stringstream ss(line);
+        vector<unsigned> ids;
 
-	const auto portNum = 60000u;
+        string tmp;
+
+        while(getline(ss, tmp, ','))
+            ids.push_back(stoul(tmp));
+
+        topologies.push_back(ids);
+        ++numberOfTests;
+    }
+    
+    // FIXME randoms shuffle
+
+	const auto defaultPortNum = 40000u;
+    auto portNumNow = defaultPortNum;
 	const auto ip = "127.0.0.1";
-
-	vector<shared_ptr<CNode> > ring;
 
 	const auto getAddress = [ip](unsigned p_port)
 	{
@@ -35,36 +47,36 @@ int main(int argc, char* argv[])
 		return ss.str();
 	};
 
-    auto logger = make_shared<CLogger>("log");
+	for (const auto topology : topologies)
+    {
+        vector<shared_ptr<CNode> > ring;
+        auto logger = make_shared<CLogger>("log");
+        const auto nodeCount = topology.size();
 
-	// creating nodes
-	for (auto i = 0u; i < nodeCount; ++i)
-	{
-        const auto rightId = (i + 1) % nodeCount;
-        const auto leftId = (i + nodeCount - 1) % nodeCount;
-		ring.emplace_back(make_shared<CNode>(i,
-                    logger,
-					getAddress(portNum + i), // right skeleton addr
-					getAddress(portNum + i + nodeCount), // left skeleton addr
-					CUnit::SNeighbour{rightId, getAddress(portNum + rightId)}, // right neighbor
-					CUnit::SNeighbour{leftId, getAddress(portNum + leftId + nodeCount)})); // left neighbor
-	}
+        for (auto i = 0u; i < nodeCount; ++i)
+        {
+            const auto rightOrderId = (i + 1) % nodeCount;
+            const auto leftOrderId = (i + nodeCount - 1) % nodeCount;
+            ring.emplace_back(make_shared<CNode>(topology[i],
+                        logger,
+                        getAddress(portNumNow + i), // right skeleton addr
+                        getAddress(portNumNow + i + nodeCount), // left skeleton addr
+                        CUnit::SNeighbour{topology[rightOrderId], getAddress(portNumNow + rightOrderId)}, // right neighbor
+                        CUnit::SNeighbour{topology[leftOrderId], getAddress(portNumNow + leftOrderId + nodeCount)})); // left neighbor
+        }
 
-	// starting skeletons
-	for (auto node : ring)
-		node->StartSkeleton();
-	// starting stubs
-	for (auto node : ring)
-		node->StartStub();
-	// starting stubs
-	for (auto node : ring)
-		node->RunHDAEagerLE();
+        portNumNow = portNumNow + 2 * nodeCount;
 
-    cout << "Waiting for the result";
-
-	// wait for keypress
-	int x;
-	cin >> x;
+        // starting skeletons
+        for (auto node : ring)
+            node->StartSkeleton();
+        // starting stubs
+        for (auto node : ring)
+            node->StartStub();
+        // starting stubs
+        for (auto node : ring)
+            node->RunHDAEagerLE();
+    }
 
 	return 0;
 }
